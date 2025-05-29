7560f47589a748620d82e3e57a0947c59a97019a
Eduardo Vilar <eduardo.vilar@pinpag.com.br>
2025-05-29 20:01:48 -0300

    Adiciona arquivo main.go com comentários sobre o uso de contexto em requisições HTTP

---
+++ context/hotel/main.go
@@ -0,0 +1,12 @@
+package main
+
+// O contexto no http é utilizado para gerenciar o contexto de uma requisição HTTP, como tempo limite e cancelamento.
+// O pacote "context" é usado para criar e gerenciar contextos que podem ser passados entre funções e goroutines.
+// Ele permite definir prazos, cancelar operações e compartilhar informações entre diferentes partes do código.
+// O contexto é especialmente útil em operações assíncronas, como requisições HTTP, onde você pode querer cancelar uma operação se ela demorar muito ou se não for mais necessária.
+import (
+
+)
+
+func main() {
+}
